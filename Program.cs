ce060d56a32354d2c6b86a71fc414af4fa1404ef
Uche Ali <uchesolomon61@gmail.com>
2026-03-18 16:40:21 +0100

    Created the program class and Main method to bring everything together

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace LibraryManagementSystem
+{
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       }
+
+ 
+}
+
+} 
